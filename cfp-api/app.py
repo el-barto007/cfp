c797f33739b86034e30b177341d611fdc3169bfd
Javier Rodriguez <javier.rodriguez@intelligential.tech>
2022-09-29 13:42:59 -0600

    hola

--- cfp-api/app.py
+++ cfp-api/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_aws101_root():
-    return jsonify(message='Hola AWS CodePipeline hola a todos')
+    return jsonify(message='Hola AWS CodePipeline HOLA grupo')
 
 
 @app.route('/voluntarios')
